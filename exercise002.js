eb878585b2f9594a0118c3a5274ad8b2425e998e
Daniyal Ahmed Kiani <86118341+deekay11@users.noreply.github.com>
2023-03-10 17:51:01 +0000

    feat: Add functionality to call different APIs

--- exercise002.js
+++ exercise002.js
@@ -1,10 +1,12 @@
 import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
+const randomJokeAPI = "https://official-joke-api.appspot.com/jokes/random";
 
 const fetchData = async (apiEndPoint) => {
   try {
     const response = await fetch(apiEndPoint);
+    //console.log(response);
     const json = await response.json();
     console.log(json);
   } catch (error) {
@@ -13,3 +15,4 @@ const fetchData = async (apiEndPoint) => {
 };
 
 fetchData(jsonTypicode);
+fetchData(randomJokeAPI);
